c2ae7e7bdfe132395afd7faea7f8ba6a224530f9
Nazarii Krypiak <nazarkryp@gmail.com>
2016-11-06 05:12:25 +0200

    changed http to https

--- src/app/shared/const.service.js
+++ src/app/shared/const.service.js
@@ -1,5 +1,5 @@
 angular.module('blogapp')
-    .constant('BASE_ADDRESS', 'http://kryptogram.azurewebsites.net/')
+    .constant('BASE_ADDRESS', 'https://kryptogram.azurewebsites.net/')
     .service("ConstService", ["BASE_ADDRESS", function (BASE_ADDRESS) {
         this.baseAddress = BASE_ADDRESS;
     }]);
