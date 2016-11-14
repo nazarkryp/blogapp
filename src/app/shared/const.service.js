09dce73780424637172d47fab6ff018c6f59f5a8
Nazarii Krypiak <nazarkryp@gmail.com>
2016-11-14 15:58:34 +0200

    changed base address uri

--- src/app/shared/const.service.js
+++ src/app/shared/const.service.js
@@ -1,6 +1,6 @@
 angular.module('blogapp')
-    // .constant('BASE_ADDRESS', 'https://kryptogram.azurewebsites.net/')
-    .constant('BASE_ADDRESS', 'http://localhost:36776/')
+    .constant('BASE_ADDRESS', 'https://kryptogram.azurewebsites.net/')
+    // .constant('BASE_ADDRESS', 'http://localhost:36776/')
     .service("ConstService", ["BASE_ADDRESS", function (BASE_ADDRESS) {
         this.baseAddress = BASE_ADDRESS;
     }]);
