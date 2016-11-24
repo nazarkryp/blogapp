0c6e245b8a5bec9f9a90457526df9e3686b37555
Nazarii Krypiak <nazarkryp@gmail.com>
2016-11-24 18:34:35 +0200

    Searchbar/post details

--- src/app/components/post/post.controller.js
+++ src/app/components/post/post.controller.js
@@ -1,19 +1,24 @@
 angular.module('blogapp')
     .controller('PostController', ['$scope', 'PostsService', 'AuthService', 'DateCalculatorService', 'post',
-        function($scope, PostsService, AuthService, DateCalculatorService, post) {
+        function ($scope, PostsService, AuthService, DateCalculatorService, post) {
             $scope.post = post;
             $scope.isAuthenticated = AuthService.authenticated;
             $scope.currentuserId = AuthService.userId;
 
-            $scope.difference = function(postDate) {
+            $scope.maxHeight = window.innerHeight * 75 / 100;
+            $scope.maxWidth = window.innerWidth * 75 / 100 - 400;
+
+            console.log($scope.maxHeight);
+
+            $scope.difference = function (postDate) {
                 return DateCalculatorService.getDifference(postDate);
             };
 
-            $scope.viewMoreCommentsClick = function(post) {
+            $scope.viewMoreCommentsClick = function (post) {
                 PostsService.getComments(post.Id).then(
-                    function(response) {
+                    function (response) {
                         post.Comments = response;
-                    }, function(error) {
+                    }, function (error) {
                         console.log(error);
                     }
                 );
--- src/app/components/search/search.controller.js
+++ src/app/components/search/search.controller.js
@@ -1,33 +1,10 @@
 angular.module('blogapp')
-    .controller('SearchController', ['$scope', '$timeout', '$q', 'SearchService',
-        function ($scope, $timeout, $q, SearchService) {
-            $scope.states = loadAll();
+    .controller('SearchController', ['$scope', '$state', '$q', 'SearchService',
+        function($scope, $state, $q, SearchService) {
             $scope.selectedItem = null;
             $scope.searchText = null;
-            $scope.querySearch = searchUsers;
 
-            // $scope.searchTextChange = function (searchQuery) {
-            //     $scope.users = [];
-
-            //     if (searchQuery) {
-            //         SearchService.search(searchQuery).then(function (response) {
-            //             $scope.users = response;
-            //         });
-            //     } else {
-            //         $scope.users = [];
-            //     }
-            // };
-
-            function querySearch(query) {
-                var results = query ? $scope.states.filter(createFilterFor(query)) : $scope.states;
-                var deferred = $q.defer();
-
-                $timeout(function () { deferred.resolve(results); }, Math.random() * 1000, false);
-
-                return deferred.promise;
-            }
-
-            function searchUsers(searchQuery) {
+            $scope.search = function(searchQuery) {
                 if (searchQuery) {
                     return SearchService.search(searchQuery);
                 }
@@ -38,29 +15,10 @@ angular.module('blogapp')
                 return deferred.promise;
             }
 
-            function loadAll() {
-                var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-
-                return allStates.split(/, +/g).map(function (state) {
-                    return {
-                        value: state.toLowerCase(),
-                        display: state
-                    };
-                });
-            }
-
-            function createFilterFor(query) {
-                var lowercaseQuery = angular.lowercase(query);
-
-                return function filterFn(state) {
-                    return (state.value.indexOf(lowercaseQuery) === 0);
-                };
-
-            }
+            $scope.selectedItemChange = function(selectedItem) {
+                if (selectedItem) {
+                    $state.go('usersfeed', { username: selectedItem.Username });
+                    $scope.selectedItem = null;
+                }
+            };
         }]);
